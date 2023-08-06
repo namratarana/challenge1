8dd5289a8947cea7de79757f480329074b0b467c
namrata <namratarana9@gmail.com>
2023-08-06 12:09:37 +0530

    first commit

---
+++ src/Components/LoginPage.js
@@ -0,0 +1,76 @@
+import React, {useState} from "react";
+import '../Components/LoginPage.css';
+import fb from "../Images/facebook.png"
+import google from "../Images/google.png"
+
+function LoginPage()
+{
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+    const [emailErr, setEmailErr] = useState("");
+    const [pwErr, setPWerr] = useState("");
+
+    const validateForm = (e) =>
+    {
+        e.preventDefault();
+         if(email=="")
+         {
+            setEmailErr("Invalid Email !");
+            return;
+         }
+         if(password=="")
+         {
+            setPWerr("Invalid Password");
+            return;
+         }
+         setEmail("");
+         setPassword("");
+    }       
+    return(
+        <div className="login-bg">
+            <div className="login-box">
+                <header className="header"></header>
+                
+                <p className="login-text"> Login </p>
+
+                <div className="login-img"/>
+
+                    <form onSubmit={validateForm} action="/">
+                        <label htmlFor="email" className="form-labels"> Email</label>
+                        <input type="email" value={email} onChange={e=> setEmail(e.target.value)}/>
+                        {emailErr==""?null: <p className="error-msg">{emailErr}</p> }
+
+
+                        <label htmlFor="password" className="form-labels"> Password</label>
+                        <input type="password" value={password} onChange={e=> setPassword(e.target.value)}/>
+                        {pwErr==""?null: <p className="error-msg">{pwErr}</p> }
+                        
+                        <button className="submit-button">
+                            Sign In
+                        </button>
+
+                        <span id="forgot-pw">Forgot Password?</span>
+                        <span id="signup">New User? Sign Up</span>
+                        <span id="or">or</span>
+
+                        <button className="blue-buttons" id="button1">
+                        <img style={{position:"absolute", left: "0.2rem"}} className="icon1" src={google} />
+                            <p>
+                                CONTINUE WITH GOOGLE
+                            </p>
+                        </button>
+                        <button className="blue-buttons" id="button2">
+                            <img style={{position:"absolute", left: "0.2rem"}} src={fb} className="icon2" />
+                            {/* <div id="icon2"> */}
+                               <p> CONTINUE WITH FACEBOOK   </p>
+                            {/* </div>  */}
+                        </button>
+                    </form>
+
+                    
+            </div>
+        </div>
+    )
+}
+
+export default LoginPage;
